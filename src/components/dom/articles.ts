80c93cc76d71b77cc8118e5c7fbc7e74f7d6d7d9
matt <matt@dont-ping.me>
2023-11-17 19:22:45 -0800

    feat: add comp-api blog

--- src/components/dom/articles.ts
+++ src/components/dom/articles.ts
@@ -1,22 +1,5 @@
 const Posts = [
     {
-        name: "Computational APIs",
-        desc: "A brief introduction to computation APIs outside of the realm of basic web-development, using Wolfram.",
-        authors: [
-            { name: "Matthew", img: "https://github.com/punctuations.png", link: "https://github.com/punctuations" }
-        ],
-        file: "/p/computational-apis",
-        id: 'compapi',
-        date: new Date(),
-        get prefferd_image(): string {
-            return this.images.static;
-        },
-        images: {
-            static: "/articles/api_static.webp",
-            dynamic: "https://www.wolframcloud.com/obj/mattsnaccc/api/moon"
-        }
-    },
-    {
         name: "Cameras & Planes",
         desc: "An introduction to planes and vector projection through a combination of Math and Comp. Sci.",
         authors: [
@@ -32,7 +15,24 @@ const Posts = [
             static: "/articles/projection_static.webp",
             dynamic: null
         }
-    }
+    },
+    {
+        name: "Computational APIs",
+        desc: "A brief introduction to computation APIs outside of the realm of basic web-development, using Wolfram.",
+        authors: [
+            { name: "Matthew", img: "https://github.com/punctuations.png", link: "https://github.com/punctuations" }
+        ],
+        file: "/p/computational-apis",
+        id: 'compapi',
+        date: new Date('Nov 17, 2023'),
+        get prefferd_image(): string {
+            return this.images.static;
+        },
+        images: {
+            static: "/articles/api_static.webp",
+            dynamic: "https://www.wolframcloud.com/obj/mattsnaccc/api/moon"
+        }
+    },
 ]
 
 export default Posts;
