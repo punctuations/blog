54030059f282d3b8b8eb95bdc975993b7d0ae9d9
matt <matt@dont-ping.me>
2023-12-15 07:49:41 -0800

    refactor(vec): rewrite post

--- src/components/blog/Split.tsx
+++ src/components/blog/Split.tsx
@@ -1,3 +1,3 @@
 export default function Split({ children }) {
-	return <section className='grid grid-cols-2'>{children}</section>;
+	return <section className='grid grid-cols-2 place-content-center justify-items-center'>{children}</section>;
 }
